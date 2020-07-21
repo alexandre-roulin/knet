f675f1003910bcf06896e1781f4064fc71c294fc
Krystalz42 <aroulin@stundent.42.fr>
2020-07-21 14:36:55 +0200

    Update trait comment

--- src/network/trait_knet.rs
+++ src/network/trait_knet.rs
@@ -1,8 +1,14 @@
 use super::*;
 pub trait KnetTransform: Copy + Sized {
+    ///Serialize the data into a vector of byte
     fn serialize(&self) -> ::std::vec::Vec<u8>;
+    ///Deserialize the data into a vector of byte
     fn deserialize(&mut self, _: &[u8]);
-    fn from_raw(_: &[u8]) -> Self;
-    fn get_size_of_data(_: &[u8]) -> usize;
+    ///Get the size of the payload of the serialize data
     fn get_size_of_payload() -> usize;
+    /// Get size of the data followed by the payload
+    /// Note the `data` have to be the same size as the `get_size_of_payload` function
+    fn get_size_of_data(data: &[u8]) -> usize;
+    ///Create `Self` by the vector of byte
+    fn from_raw(_: &[u8]) -> Self;
 }
